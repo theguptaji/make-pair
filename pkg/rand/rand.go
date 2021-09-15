b7d36e4179587c7e78c5bfb35f57cd8c7c1a0057
Aman Gupta <aman@keio.jp>
2021-09-15 09:25:05 +0530

    add rooms

--- pkg/rand/rand.go
+++ pkg/rand/rand.go
@@ -8,8 +8,10 @@ import (
 
 func RandomPairs(members []string, maxMembers int) {
 	var newList []string
-
+	type team []string
+	var teams []team
 	//teams := []rune{'A', 'B', 'C', 'D'}
+	rooms := []string{"Room B", "Room C", "Room D", "Room E", "Room F", "Room G"}
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
 	memberCount := len(members)
@@ -18,12 +20,18 @@ func RandomPairs(members []string, maxMembers int) {
 		newList = append(newList, members[v])
 	}
 	for i := 0; i < memberCount; i += maxMembers {
-		if i+2*maxMembers < memberCount {
-			fmt.Println(newList[i : i+maxMembers])
+		if i+2*maxMembers <= memberCount {
+			teams = append(teams, newList[i:i+maxMembers])
+			//fmt.Println(newList[i : i+maxMembers])
 		} else {
-			fmt.Println(newList[i:])
+			teams = append(teams, newList[i:])
+			//fmt.Println(newList[i:])
 			break
 		}
+	}
+	for i, v := range teams {
+		fmt.Print(rooms[i] + " ")
+		fmt.Println(v)
 	}
 
 }
